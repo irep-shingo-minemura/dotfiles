679730fb7d0e51e417111faaa5baaabda417d798
irep-shingo-minemura <shingo_minemura@irep.co.jp>
2022-02-04 17:51:49 +0900

    for remotecontainer

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/zsh
+
+cd $(dirname $0)
+
 for name in *; do
     [ "$name" = "install.sh" ] && continue
     [ "$name" = "LICENSE" ] && continue
     [ "$name" = "README.md" ] && continue
     [ "$name" = ".DS_Store" ] && continue
     [ "$name" = "com.googlecode.iterm2.plist" ] && continue
+
+    if "$REMOTE_CONTAINERS" = "true"; then
+        [ "$name" = "config/karabiner"* ] && continue
+        [ "$name" = "Brewfile" ] && continue
+        [ "$name" = "gitignore_global" ] && continue
+    fi
 
     # 上書きはしない
     target="$HOME/.$name"
@@ -13,3 +22,7 @@ for name in *; do
         echo "Created $name"
     fi
 done
+
+if "$REMOTE_CONTAINERS" = "true"; then
+    curl -s "https://starship.rs/install.sh" | sh -s -- -y
+fi
