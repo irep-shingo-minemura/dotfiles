bb998371b4352c63a4cf826d4cf258157839ced7
irep-shingo-minemura <shingo_minemura@irep.co.jp>
2022-02-18 17:42:57 +0900

    add devcontainer installer

---
+++ devcontainer.sh
@@ -0,0 +1,17 @@
+cd $(dirname $0)
+
+for name in *; do
+    [ "$name" != "config/starship.toml" ] && continue
+    [ "$name" = ".gitignore" ] && continue
+    [ "$name" = ".gitmodules" ] && continue
+    [ "$name" = "Brewfile" ] && continue
+    [ "$name" = "com.googlecode.iterm2.plist" ] && continue
+    [ "$name" = "devcontainer.sh" ] && continue
+    [ "$name" = "install.sh" ] && continue
+    [ "$name" = "README.md" ] && continue
+    [ "$name" = ".DS_Store" ] && continue
+
+    ln -s "$PWD/$name" "$target"
+done
+
+curl -s "https://starship.rs/install.sh" | sh -s -- -y
