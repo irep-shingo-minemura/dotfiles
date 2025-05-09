9e270a222b734ace0a111746e4a4e1f810997af0
Shingo Minemura <shingo.minemura@hakuhodo-technologies.co.jp>
2025-05-09 16:16:41 +0900

    feat: install node

--- devcontainer-setup.sh
+++ devcontainer-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # inshellisense
+if ! command -v node > /dev/null 2>&1; then
+    apt install -y nodejs npm
+fi
+
 npm install -g @microsoft/inshellisense
 is init bash >> ~/.bashrc
 echo "" >> ~/.bashrc # 上記のコマンドの後ろに改行無しで別のコマンドが追記されるので明示的に改行する
